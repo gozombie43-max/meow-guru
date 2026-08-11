76029a80be62504d1319cf2dae2c423e55f3260c
Gurucharan <gozombie43@gmail.com>
2026-08-11 11:10:08 +0530

    test commit

--- backend/index.js
+++ backend/index.js
@@ -70,7 +70,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options(/(.*)/, cors(corsOptions)); // handle preflight for all routes
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
