44403802fe978c07690a99cad83350d7d6fd156c
GURUCHARAN MURMU <gozombie43@gmail.com>
2026-09-09 17:43:06 +0530

    perf(f1): stop background frontend wakeups

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -1,30 +1,16 @@
 import AppRecovery from '@/components/AppRecovery';
-import AppWarmup from '@/components/AppWarmup';
 import BottomNav from '@/components/BottomNav';
 import FeedbackToast from '@/components/FeedbackToast';
 import PageTransitionShell from '@/components/PageTransitionShell';
-import PushOpenAnalyticsBridge from '@/components/PushOpenAnalyticsBridge';
-import PushRegistrationBridge from '@/components/PushRegistrationBridge';
-import StudyTimeTracker from '@/components/StudyTimeTracker';
 import { AuthProvider } from '@/context/AuthContext';
-import { NotificationCenterProvider } from "@/context/NotificationCenterContext";
-import { StudyTelemetryProvider } from "@/context/StudyTelemetryContext";
 
 export default function StudyLayout({ children }: { children: React.ReactNode }) {
- return (
-        <AuthProvider>
-          <PushRegistrationBridge />
-          <PushOpenAnalyticsBridge />
-          <NotificationCenterProvider>
-            <StudyTelemetryProvider>
-              <StudyTimeTracker />
-              <AppRecovery />
-              <AppWarmup />
-              <PageTransitionShell>{children}</PageTransitionShell>
-              <BottomNav />
-              <FeedbackToast />
-            </StudyTelemetryProvider>
-          </NotificationCenterProvider>
-        </AuthProvider>
- );
+  return (
+    <AuthProvider>
+      <AppRecovery />
+      <PageTransitionShell>{children}</PageTransitionShell>
+      <BottomNav />
+      <FeedbackToast />
+    </AuthProvider>
+  );
 }
